f1603e3ccf9691622d4427d1c3d118b0bdbabb3e
Juozas Gaigalas <juozasgaigalas@gmail.com>
2011-10-23 21:48:43 -0700

    unicorn config

---
+++ config/unicorn.rb
@@ -0,0 +1,44 @@
+# Config from http://unicorn.bogomips.org/examples/unicorn.conf.rb
+
+# See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete
+# documentation.
+
+app_name = "members.workersolidarity.org"
+app_path = "/var/www/#{app_name}"
+
+worker_processes 4
+
+working_directory app_path
+
+# listen on both a Unix domain socket and a TCP port,
+# we use a shorter backlog for quicker failover when busy
+listen "/tmp/#{app_name}.unicorn.sock", :backlog => 64
+listen 8080, :tcp_nopush => true
+
+# nuke workers after 30 seconds instead of 60 seconds (the default)
+timeout 30
+
+# feel free to point this anywhere accessible on the filesystem
+pid "/tmp/pids/unicorn.pid"
+
+stderr_path "/var/log/unicorn/#{app_name}.stderr.log"
+stdout_path "/var/log/unicorn/#{app_name}.stdout.log"
+
+# combine REE with "preload_app true" for memory savings
+# http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
+preload_app true
+GC.respond_to?(:copy_on_write_friendly=) and
+  GC.copy_on_write_friendly = true
+
+before_fork do |server, worker|
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  # the following is *required* for Rails + "preload_app true",
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
