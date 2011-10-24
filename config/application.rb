e97a01a319208bb3e66a9f8687603f481cc5c393
Juozas Gaigalas <juozasgaigalas@gmail.com>
2011-10-24 16:42:18 -0700

    fixed some assets path problems

--- config/application.rb
+++ config/application.rb
@@ -50,7 +50,7 @@ module Members
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
+    config.assets.precompile += ['inline_editor.js']
 
 
     # MAILER
