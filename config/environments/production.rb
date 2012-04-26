931dc06b12855cf82f053ce515c901b318067542
Juozas Gaigalas <juozasgaigalas@gmail.com>
2012-04-26 15:04:28 -0700

    mailer paths

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,8 @@ MembersSite::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { :host => 'members.workersolidarity.org' }
+
   # Enable threaded mode
   # config.threadsafe!
 
