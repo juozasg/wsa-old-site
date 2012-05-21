56b5dc90aa40d5f916f44a4e1c509e214f01d931
Juozas Gaigalas <juozasgaigalas@gmail.com>
2012-05-20 20:32:24 -0700

    config updates form generate refinery:cms

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ end
 
 module MembersSite
   class Application < Rails::Application
+    # Refinery has set config.assets.initialize_on_precompile = false by default.
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
