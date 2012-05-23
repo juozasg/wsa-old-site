de8bdebc6754711d23f2861bad59cef78ca47a8d
Juozas Gaigalas <juozasgaigalas@gmail.com>
2012-05-22 17:33:20 -0700

    remove non-useful forem initializer line

--- config/initializers/forem.rb
+++ config/initializers/forem.rb
@@ -5,4 +5,3 @@ Forem.user_profile_links = true
 Forem.email_from_address = "admin@members.workersolidarity.org"
 Forem.formatter = Forem::Formatters::HTML
 Forem.avatar_user_method = "avatar_url" # Method called on user to return an image url for a non-gravatar custom avatar
-Forem.per_page = 20
