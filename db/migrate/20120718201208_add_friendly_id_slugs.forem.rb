548834b9fd9cc8e2d905261d3095aac4bf3dde69
Juozas Gaigalas <juozasgaigalas@gmail.com>
2012-07-19 12:08:36 -0700

    include migration fixes from forem

--- db/migrate/20120718201208_add_friendly_id_slugs.forem.rb
+++ db/migrate/20120718201208_add_friendly_id_slugs.forem.rb
@@ -4,16 +4,16 @@ class AddFriendlyIdSlugs < ActiveRecord::Migration
     add_column :forem_forums, :slug, :string
     add_index :forem_forums, :slug, :unique => true
     Forem::Forum.reset_column_information
-    Forem::Forum.all.each {|t| t.save! }
+    Forem::Forum.find_each {|t| t.save! }
 
     add_column :forem_categories, :slug, :string
     add_index :forem_categories, :slug, :unique => true
     Forem::Category.reset_column_information
-    Forem::Category.all.each {|t| t.save! }
+    Forem::Category.find_each {|t| t.save! }
 
     add_column :forem_topics, :slug, :string
     add_index :forem_topics, :slug, :unique => true
     Forem::Topic.reset_column_information
-    Forem::Topic.all.each {|t| t.save! }
+    Forem::Topic.find_each {|t| t.save! }
   end
 end
