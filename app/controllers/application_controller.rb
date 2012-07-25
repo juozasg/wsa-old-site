30f8de7809bed7e90c2b01e8a0869b3a612cf1a9
Juozas Gaigalas <juozasgaigalas@gmail.com>
2012-07-25 11:48:24 -0700

    Everything on the website requires logging in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  # append_before_filter :force_login_user
+  append_before_filter :authenticate_refinery_user!
 
 
   def forem_user
@@ -11,14 +11,18 @@ class ApplicationController < ActionController::Base
     current_refinery_user
   end
 
+  def after_sign_in_path_for(resource)
+    session["refinery_user_return_to"] || '/'
+  end
+
   protected
 
-  def force_login_user
-    unless current_refinery_user.try(:has_role?, "Member")
-      session['refinery_user_return_to'] = request.fullpath
-      # redirect_to refinery.new_refinery_user_session_path
-    end
-  end
+  # def force_login_user
+  #   unless current_refinery_user.try(:has_role?, "Member")
+  #     session['refinery_user_return_to'] = request.fullpath
+  #     # redirect_to refinery.new_refinery_user_session_path
+  #   end
+  # end
 
   helper_method :forem_user
 end
