ed5edb547900dceb73c68bd8c29ed7e8a046dbae
Juozas Gaigalas <juozasgaigalas@gmail.com>
2012-07-28 04:24:06 -0500

    prettier emails for mailers

--- config/initializers/forem.rb
+++ config/initializers/forem.rb
@@ -1,4 +1,4 @@
 Forem.user_class = "Refinery::User"
 Forem.user_profile_links = true
-Forem.email_from_address = "admin@members.workersolidarity.org"
+Forem.email_from_address = '"WSA Members Site" <no-reply@members.workersolidarity.org>'
 Forem.avatar_user_method = "avatar_url" # Method called on user to return an image url for a non-gravatar custom avatar
