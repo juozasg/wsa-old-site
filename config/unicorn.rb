d0666177e9a9628358a0c35f707b28271403e22e
Juozas Gaigalas <juozasgaigalas@gmail.com>
2012-08-02 18:01:02 -0400

    change unicorn timeout

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -16,7 +16,7 @@ listen "/tmp/#{app_name}.unicorn.sock", :backlog => 64
 listen 8080, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
-timeout 30
+timeout 60
 
 # feel free to point this anywhere accessible on the filesystem
 pid "/var/run/unicorn.pid"
